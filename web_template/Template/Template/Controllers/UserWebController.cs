d399cd8e4f2c16ed699a33014e6296ad618eba26
Muhammad Zaid <zaid123gitub@gmail.com>
2024-09-05 13:38:49 +0500

    Contact us feedback mailer done

--- web_template/Template/Template/Controllers/UserWebController.cs
+++ web_template/Template/Template/Controllers/UserWebController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 
 namespace Template.Controllers
 {
@@ -8,5 +13,43 @@ namespace Template.Controllers
 		{
 			return View();
 		}
-	}
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult product_page()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(string fname, string lname, string email, string subject, string message)
+        {
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+
+            //ptmaptech@gmail.com    "ylkflsuagqvtshad"
+            client.Credentials = new NetworkCredential("zaid123aptech@gmail.com", "pkoziuhxgougqimx");
+
+            MailMessage mssg = new MailMessage(email, "zaid123aptech@gmail.com");
+            mssg.Subject = "Feedback";
+            mssg.Body = fname + " " + lname + " " + email + " " + subject + " " + message;
+
+            // msg.Attachments.Add(new Attachment(PathToAttachment));
+            client.Send(mssg);
+
+            ViewBag.message = "Mail sent successfully, Role assigned";
+
+            return View();
+        }
+    }
 }
