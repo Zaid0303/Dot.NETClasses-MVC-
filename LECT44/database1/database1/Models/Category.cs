e00e9810ef0bb946073bb5c710e633ac58b71a0a
Muhammad Zaid <zaid123gitub@gmail.com>
2024-09-24 14:24:37 +0500

    Products from database

---
+++ LECT44/database1/database1/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace database1.Models;
+
+public partial class Category
+{
+    public int CatId { get; set; }
+
+    public string? CatName { get; set; }
+}
---
+++ LECT44/database1/database1/Models/EmpContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace database1.Models;
+
+public partial class EmpContext : DbContext
+{
+    public EmpContext()
+    {
+    }
+
+    public EmpContext(DbContextOptions<EmpContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Login> Logins { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("data source=.; initial catalog=emp; user id=sa; password=aptech; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CatId).HasName("PK__Category__26E351409DBD9EF2");
+
+            entity.ToTable("Category");
+
+            entity.Property(e => e.CatId).HasColumnName("Cat_Id");
+            entity.Property(e => e.CatName)
+                .HasMaxLength(10)
+                .HasColumnName("Cat_Name");
+        });
+
+        modelBuilder.Entity<Login>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__login__3214EC07CE89C6DF");
+
+            entity.ToTable("login");
+
+            entity.Property(e => e.Password)
+                .HasMaxLength(250)
+                .HasColumnName("password");
+            entity.Property(e => e.Roleid).HasColumnName("roleid");
+            entity.Property(e => e.Username)
+                .HasMaxLength(250)
+                .HasColumnName("username");
+
+            entity.HasOne(d => d.Role).WithMany(p => p.Logins)
+                .HasForeignKey(d => d.Roleid)
+                .HasConstraintName("FK_login_ToTable");
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Role__3214EC07AF0DBE76");
+
+            entity.ToTable("Role");
+
+            entity.Property(e => e.Rname)
+                .HasMaxLength(250)
+                .HasColumnName("rname");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
